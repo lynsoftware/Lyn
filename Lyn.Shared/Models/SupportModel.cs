35afa934b1a6adee84503d4cf16c4bcc35e4e2a0
lynsoftware <dev@lynsoftware.com>
2026-01-13 15:05:38 +0100

    Support side og testingside

---
+++ Lyn.Shared/Models/SupportModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lyn.Shared.Models;
+
+public class SupportModel
+{
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(100, ErrorMessage = "Title cant be longer than 100 characters")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please choose a category")]
+    public string Category { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Description is required")]
+    [StringLength(2000, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 2000 characters")]
+    public string Description { get; set; } = string.Empty;
+}
